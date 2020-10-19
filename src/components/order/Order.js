5ff2ba134e3b0fd4e6cf7deed370008404abc85a
pavel.mikhailov <pavel.mikhailov@office.e2e4gu.ru>
2020-10-19 17:17:25 +0700

    To create a library for interacting for dom element. Refactoring.

--- src/components/order/Order.js
+++ src/components/order/Order.js
@@ -1,3 +1,5 @@
+import {$} from '@core/dom'
+
 export class Order {
   constructor(selector, options) {
     this.$el = document.querySelector(selector)
@@ -6,11 +8,9 @@ export class Order {
   }
 
   getRoot() {
-    const $root = document.createElement('div')
-    $root.classList.add('order')
+    const $root = $.create('div', 'order')
     this.components.forEach(Component => {
-      const $el = document.createElement('div')
-      $el.classList.add(Component.className)
+      const $el = $.create('div', Component.className)
       const component = new Component($el, this.childComponents)
       $el.innerHTML = component.toHTML()
       $root.append($el)
