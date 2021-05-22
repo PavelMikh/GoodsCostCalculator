5b30934ecf4c62b6d226849f7e0898430e54a63a
pavel.mikhailov <pamix1985@gmail.com>
2021-05-23 02:33:33 +0700

    data-server change

--- data_service/function.js
+++
@@ -1,9 +0,0 @@
-import http from 'http';
-import fs from 'fs';
-
-export const download = (url, dest, cb) => {
-  const file = fs.createWriteStream(dest);
-  const request = http.get(url, (response) => {
-    response.pipe(file);
-  })
-}
